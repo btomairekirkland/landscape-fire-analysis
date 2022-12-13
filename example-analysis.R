e760930fa0f1b56027260398a770e56ef8278a7c
Máire Kirkland <103254229+btomairekirkland@users.noreply.github.com>
2022-12-13 16:21:37 +0000

    Updat plotting code and add model weights

--- example-analysis.R
+++ example-analysis.R
@@ -5,9 +5,10 @@ rm(list = ls())
 
 # Load libraries
 library(mgcv)
-library(corrplot)
-library(RColorBrewer)
-library(itsadug)
+library(interactions)
+library(ggplot2)
+library(gridExtra)
+library(dplyr)
 
 # Read in your gridded fire dataset
 setwd("~/BTO projects/Polesia wildfires")
@@ -51,35 +52,66 @@ corrplot(tmp, type="upper", diag = F, # Add coefficient of correlation
          tl.col="black", tl.cex = .7, p.mat = p.mat, sig.level = .05, insig = "blank",
          col=brewer.pal(n = 8, name = "PuOr")) # Text label color and rotation
 
-# Add your offset term 
-ofs <- 5e-0 # Change this based on your sample of non-fire observations
-pix$offset <- ifelse(pix$burn == 1, -log(1), -log(ofs)) # 1 for fire observations
+# Add weight for each land cover type
+## This is a dataframe with the weights to be used in the model, which reflects the proportion of each land cover type sampled 
+## One row for each voxel with the weight assigned to the land cover type at the centre point of the voxel 
+weights <- read.csv("model-weights.csv")
+df <- inner_join(df, weights)
 
-# Run binomial GAM with non-collinear variables
-## bam used for large datasets
-## X and Y are centroid coordinates for voxels to account for spatial dependence
-## n_year is an integer column representing the year numerically, starting from 1 
-## day is an integer column representing the day of the year to account for temporal dependence
-mod <- bam(burn ~ s(X, Y, bs = 'ds', k = 99) + s(n_year, bs = "cr", k = 19) + s(day, k = 100, bs = "cc") + 
-             s(Agriculture, bs = 'cr', k = 20) + s(Raised.bog, bs = 'cr', k = 20) + s(Fen.and.transition.mire, bs = 'cr', k = 20) + 
-             s(Scrub, bs = 'cr', k = 20) + s(Deciduous.forest, bs = 'cr', k = 20) + s(Coniferous.forest, bs = 'cr', k = 20) + 
-             s(Meadows, bs = 'cr', k = 20) + s(Urban, bs = 'cr', k = 20) + s(friction, bs = 'cr', k = 20) + s(dist_road, bs = 'cr', k = 20) + 
-             s(pop_dens, bs = 'cr', k = 20) + s(ffmc, bs = 'cr', k = 20) + s(rain, bs = 'cr', k = 20) + s(dc, bs = 'cr', k = 20) + 
-             s(ndvi, bs = 'cr', k = 20) + s(fwi, bs = 'cr', k = 20) + s(max_temp, bs = 'cr', k = 20),
-           offset = offset, data = pix, family = binomial, discrete = T, select = T)
+# Run model exploring burning of different key land cover types in relation to moisture 
+mod <- bam(burn ~ s(soil_moist, bs = "cr", k = 20) + ti(Meadows, soil_moist, bs = c("cr","cr")) + ti(Meadows, ffmc, bs = c("cr","cr")) + 
+             s(Raised.bog, bs = "cr", k = 20) + ti(Raised.bog, ffmc, bs = c("cr","cr")) + ti(Raised.bog, soil_moist, bs = c("cr","cr")) +
+             ti(Deciduous.forest, soil_moist, bs = c("cr","cr")) + ti(Deciduous.forest, ffmc, bs = c("cr","cr")) + 
+             s(Deciduous.forest, bs = "cr", k = 20) + s(Agriculture, bs = "cr", k = 20) + s(max_temp, bs = "cr") + s(rain, bs = "cr", k = 15) +
+             ti(Fen.and.transition.mire, soil_moist, bs = c("cr","cr")) + s(Meadows, bs = "cr", k = 20) + 
+             s(Fen.and.transition.mire, bs = "cr") + s(ffmc, bs = "cr", k = 20) + s(ndvi, bs = "cr", k = 20) + 
+             protected + fire_season + s(X, Y, bs = "ds", k = 1000) + ti(Fen.and.transition.mire, ffmc, bs = c("cr","cr")),
+           data = df[df$fire_season != "Non-fire season",], weights = weight, discrete = T, gamma = 1.4, select = T, family = binomial)
 
-# Check residuals and values of k
+# Check model output
+summary(mod)
+
+# Check model residuals
 par(mfrow=c(2,2))
 gam.check(mod)
 dev.off()
 
-# Explore model output 
-summary(mod)
+# Predict probability of burning and plot effects
 
-# Plot individual effects using temperature as an example
-plot_smooth(mod, view = "Agriculture", 
-            rm.ranef = F, transform = plogis, # Plot effects on response scale 
-            ylim = c(0,1),
-            xlab = "Proportion of agriculture", 
-            ylab = "Probability of fire", 
-            h0 = NULL)
+# Example based on fen and transition mires and soil moisture 
+
+# First create new dataframe for predictions
+new.df <- expand.grid(Fen.and.transition.mire = seq(0,1,by=.001), 
+                              soil_moist = c(23,57,82), ## Mean and quartile values 
+                              protected = "yes",
+                              fire_season = "Early",
+                              Agriculture = mean(df$Agriculture),
+                              ffmc = mean(df$ffmc),
+                              Raised.bog = mean(df$Raised.bog),
+                              Deciduous.forest = mean(df$Deciduous.forest),
+                              ndvi = mean(df$ndvi),
+                              max_temp = mean(df$max_temp), 
+                              rain = mean(df$rain),
+                              Meadows = mean(df$Meadows),
+                              X = mean(df$X),
+                              Y = mean(df$Y)) 
+
+# Then predict to new dataframe and calculate confidence intervals
+new.df[,15:16]<- predict(mod, newdata=new.df,  type = "response", se = TRUE)
+# Convert standard error to confidence intervals
+new.df$upr <- new.df$fit + (2 * new.df$se.fit)
+new.df$lwr <- new.df$fit - (2 * new.df$se.fit)
+
+# Produce plot
+ggplot(new.df) +
+  geom_line(aes(x = Fen.and.transition.mire, y = fit, col = as.factor(soil_moist))) +
+  geom_ribbon(aes(x = Fen.and.transition.mire, ymin = lwr, ymax = upr, fill = as.factor(soil_moist)), col = NA,
+              alpha = .3) +
+  theme_classic() +
+  scale_fill_manual(values = c('#a6bddb','#3690c0','#023858')) +
+  scale_colour_manual(values = c('#a6bddb','#3690c0','#023858')) +
+  xlab("Proportion of fen and transition mire") +
+  ylab("Probability of burning") +
+  theme(axis.title.y = element_text(margin = unit(c(0, 5, 0, 0), "mm")),
+        axis.title.x = element_text(margin = unit(c(3, 0, 0, 0), "mm")))
+
