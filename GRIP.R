b44c5d0cdeaeb73a38073aad9b6d7df5d946fb97
Máire Kirkland <103254229+btomairekirkland@users.noreply.github.com>
2022-12-14 16:55:06 +0000

    Update GRIP.R

--- GRIP.R
+++ GRIP.R
@@ -11,7 +11,10 @@ library(dplyr)
 
 # Read in shapefiles
 setwd("~/BTO projects/Polesia wildfires/fire shapefiles/pixels")
-pix <- st_read("all_pixels.shp") 
+shp <- st_read("all_pixels.shp") ## Voxel dataset
+## Can be done instead for ignition points
+## setwd("~/BTO projects/Polesia wildfires/fire shapefiles/ignition points")
+## shp <- st_read("ignition_points.shp"
 
 # Read in road shapefiles from Asia and Europe (continental shapefiles rather than global to speed up processing) and global road density raster
 roads.europe <- st_read("~/BTO projects/Polesia wildfires/input variables/GRIP4_Region4_vector_shp/GRIP4_region4.shp")
@@ -21,28 +24,28 @@ dens <- raster("~/BTO projects/Polesia wildfires/input variables/GRIP_density/gr
 # Combine both road shapefiles into one
 roads <- rbind(roads.asia, roads.europe)
 
-# Find nearest road to grid cell
-n.ptch <- st_nearest_feature(pix, roads)
+# Find nearest road to grid cell (or ignition point)
+n.ptch <- st_nearest_feature(shp, roads)
 # Create empty vector
 distances <- 0
 
-# Calculate distances between grid cells and nearest road (in m)
-for (i in 1:nrow(pix)) {
-  distances[i] <- st_distance(pix[i,], roads[n.ptch[i],])
+# Calculate distances to nearest road (in m)
+for (i in 1:nrow(shp)) {
+  distances[i] <- st_distance(shp[i,], roads[n.ptch[i],])
 }
 
 # Add distances to dataset and convert to km
 pix$dist_road <- df$distances/1000 
 
 # Extract road density 
-pix$dens_road <- extract(dens, pix, fun = mean, weights = T)
+pix$dens_road <- extract(dens, shp, fun = mean, weights = T)
 
 # Drop geometry and export as csv
-df <- st_drop_geometry(pix)
+df <- st_drop_geometry(shp)
 setwd("~/BTO projects/Polesia wildfires/input files") # Change working directory to where you are storing output files
 write.csv(df, "grid-cells-roads.csv", row.names = F) # Output file is a dataframe with grid cell IDs, distance to nearest road and road density
 
-# Can summarise for each fire patch by calculating minimum distance or repeat above with ignition shapefiles 
+# Can summarise for each fire patch by calculating minimum distance if using voxel dataset
 fires <- df[!grepl("^CP", df$z),] # Select burnt grid cells
 fires <- fires %>% 
   group_by(z) %>% 
